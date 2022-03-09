55758ef2661cb5cf843a7cd8ee90c1a0b3c0e7e0
Mene-hub <menegotto.claudio@gmail.com>
2022-03-09 13:56:43 +0100

    Initial commit

---
+++ CusomMessageBox/MMBox.xaml.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MaterialDesignColors;
+using MaterialDesignThemes.Wpf;
+
+namespace CusomMessageBox
+{
+    public partial class MMBox : Window
+    {
+        public MMBox()
+        {
+            InitializeComponent();
+        }
+
+        private MessageBoxResult defaultResult;
+        private bool defaultExist = false;
+
+        internal void setButtons(MessageBoxButton button)
+        {
+            PackIcon[] icons = new PackIcon[3];
+            icons[0] = new PackIcon();
+            icons[0].Kind = PackIconKind.Check;
+
+            icons[1] = new PackIcon();
+            icons[1].Kind = PackIconKind.DeleteForever;
+
+            icons[2] = new PackIcon();
+            icons[2].Kind = PackIconKind.Close;
+
+            switch (button)
+            {
+                case MessageBoxButton.OK:
+
+                    button0.Tag = "OK";
+                    button0.Content = icons[0];
+                    button0.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00A416"));
+
+                    button1.Visibility = Visibility.Hidden;
+
+                    button2.Visibility = Visibility.Hidden;
+
+                    break;
+
+                case MessageBoxButton.OKCancel:
+
+                    button0.Tag = "OK";
+                    button0.Content = icons[0];
+                    button0.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00A416"));
+
+                    button1.Tag = "Cancel";
+                    button1.Content = icons[1];
+                    button1.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF304FFE"));
+
+                    button2.Visibility = Visibility.Hidden;
+
+                    break;
+
+                case MessageBoxButton.YesNo:
+
+                    button0.Tag = "No";
+                    button0.Content = icons[2];
+                    button0.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFF0000"));
+
+                    button1.Tag = "Yes";
+                    button1.Content = icons[0];
+                    button1.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00A416"));
+
+                    button2.Visibility = Visibility.Hidden;
+
+                    break;
+
+                case MessageBoxButton.YesNoCancel:
+
+                    button0.Tag = "No";
+                    button0.Content = icons[2];
+                    button0.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFF0000"));
+
+                    button1.Tag = "Yes";
+                    button1.Content = icons[0];
+                    button1.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00A416"));
+
+                    button2.Tag = "Cancel";
+                    button2.Content = icons[1];
+                    button2.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF304FFE"));
+
+                    break;
+            }
+        }
+
+        internal void setNoneButtons()
+        {
+            button0.Visibility = Visibility.Hidden;
+            button1.Visibility = Visibility.Hidden;
+            button2.Visibility = Visibility.Hidden;
+            BoxIcon.Visibility = Visibility.Hidden;
+        }
+        internal void setButtons(MessageBoxButton button, MessageBoxResult defaultresult)
+        {
+            defaultExist = true;
+            defaultResult = defaultresult;
+
+            PackIcon[] icons = new PackIcon[3];
+            icons[0] = new PackIcon();
+            icons[0].Kind = PackIconKind.Check;
+
+            icons[1] = new PackIcon();
+            icons[1].Kind = PackIconKind.DeleteForever;
+
+            icons[2] = new PackIcon();
+            icons[2].Kind = PackIconKind.Close;
+
+            switch (button)
+            {
+                case MessageBoxButton.OK:
+
+                    button0.Tag = "OK";
+                    button0.Content = icons[0];
+                    button0.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00A416"));
+
+                    button1.Visibility = Visibility.Hidden;
+
+                    button2.Visibility = Visibility.Hidden;
+
+                    break;
+                case MessageBoxButton.OKCancel:
+
+                    button0.Tag = "OK";
+                    button0.Content = icons[0];
+                    button0.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00A416"));
+
+                    button1.Tag = "Cancel";
+                    button1.Content = icons[1];
+                    button1.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF304FFE"));
+
+                    button2.Visibility = Visibility.Hidden;
+
+                    break;
+                case MessageBoxButton.YesNo:
+
+                    button0.Tag = "No";
+                    button0.Content = icons[2];
+                    button0.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFF0000"));
+
+                    button1.Tag = "Yes";
+                    button1.Content = icons[0];
+                    button1.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00A416"));
+
+                    button2.Visibility = Visibility.Hidden;
+
+                    break;
+                case MessageBoxButton.YesNoCancel:
+
+                    button0.Tag = "No";
+                    button0.Content = icons[2];
+                    button0.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFF0000"));
+
+                    button1.Tag = "Yes";
+                    button1.Content = icons[0];
+                    button1.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00A416"));
+
+                    button2.Tag = "Cancel";
+                    button2.Content = icons[1];
+                    button2.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF304FFE"));
+
+                    break;
+                default:
+                    button0.Visibility = Visibility.Hidden;
+                    button1.Visibility = Visibility.Hidden;
+                    button2.Visibility = Visibility.Hidden;
+                    BoxIcon.Visibility = Visibility.Hidden;
+                    break;
+            }
+        }
+
+        internal void setIcon(MessageBoxImage image)
+        {
+            switch (image)
+            {
+                case MessageBoxImage.Error:
+                    BoxIcon.Kind = PackIconKind.Error;
+                    BoxIcon.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFF0000"));
+                    break;
+                case MessageBoxImage.Information:
+                    BoxIcon.Kind = PackIconKind.InfoCircle;
+                    BoxIcon.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF304FFE"));
+                    break;
+                case MessageBoxImage.Question:
+                    BoxIcon.Kind = PackIconKind.QuestionMarkRhombus;
+                    BoxIcon.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF304FFE"));
+                    break;
+                case MessageBoxImage.Warning:
+                    BoxIcon.Kind = PackIconKind.WarningBox;
+                    BoxIcon.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFB900"));
+                    break;
+                case MessageBoxImage.None:
+                    BoxIcon.Kind = PackIconKind.None;
+                    break;
+            }
+        }
+
+        internal void setresult(object sender, RoutedEventArgs e)
+        {
+            defaultExist = false;
+            MaterialMessageBox.result = stringToResult(((Button)sender).Tag.ToString());
+            this.Close();
+        }
+
+        internal MessageBoxResult stringToResult(string value)
+        {
+            value = value.ToUpper();
+            switch (value)
+            {
+                case "OK":
+                    return MessageBoxResult.OK;
+
+                case "CANCEL":
+                    return MessageBoxResult.Cancel;
+
+                case "YES":
+                    return MessageBoxResult.Yes;
+
+                case "NO":
+                    return MessageBoxResult.No;
+
+                default:
+                    return MessageBoxResult.None;
+            }
+        }
+
+        private void setDefault(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (defaultExist)
+            {
+                MaterialMessageBox.result = defaultResult;
+            }
+        }
+    }
+}
---
+++ CusomMessageBox/MaterialMessageBox.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CusomMessageBox
+{
+    /// <summary>
+    /// Per utilizzare questo controllo personalizzato in un file XAML, eseguire i passaggi 1a o 1b e 2.
+    ///
+    /// Passaggio 1a) Utilizzo di questo controllo personalizzato in un file XAML esistente nel progetto corrente.
+    /// Aggiungere questo attributo XmlNamespace all'elemento radice del file di markup dove 
+    /// deve essere utilizzato:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:CusomMessageBox"
+    ///
+    ///
+    /// Passaggio 1b) Utilizzo del controllo personalizzato in un file XAML esistente in un progetto diverso.
+    /// Aggiungere questo attributo XmlNamespace all'elemento radice del file di markup dove 
+    /// deve essere utilizzato:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:CusomMessageBox;assembly=CusomMessageBox"
+    ///
+    /// Sarà inoltre necessario aggiungere nel progetto in cui si trova il file XAML
+    /// un riferimento a questo progetto, quindi ricompilare per evitare errori di compilazione:
+    ///
+    ///     In Esplora soluzioni, fare clic con il pulsante destro del mouse sul progetto di destinazione, quindi scegliere
+    ///     "Aggiungi riferimento"->"Progetti"->[Selezionare questo progetto]
+    ///
+    ///
+    /// Passaggio 2)
+    /// Utilizzare il controllo nel file XAML.
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public sealed class MaterialMessageBox
+    {
+        internal static MessageBoxResult result;
+        public static MessageBoxResult Show(string messageBoxText)
+        {
+            MMBox myBox = new MMBox();
+            myBox.Content.Text= messageBoxText;
+            myBox.Title = "MaterialMessageBox";
+            myBox.setNoneButtons();
+            myBox.ShowDialog();
+            return result;
+        }
+        
+        public static MessageBoxResult Show(string messageBoxText, string caption)
+        {
+            MMBox myBox = new MMBox();
+            myBox.Content.Text = messageBoxText;
+            myBox.Title = caption;
+            myBox.setNoneButtons();
+            myBox.ShowDialog();
+            return result;
+        }
+
+        public static MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button)
+        {
+            MMBox myBox = new MMBox();
+            myBox.Content.Text = messageBoxText;
+            myBox.Title = caption;
+            myBox.setButtons(button);
+            myBox.ShowDialog();
+            return result;
+        }
+
+        public static MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon)
+        {
+            MMBox myBox = new MMBox();
+            myBox.Content.Text = messageBoxText;
+            myBox.Title = caption;
+            myBox.setButtons(button);
+            myBox.setIcon(icon);
+            myBox.ShowDialog();
+            return result;
+        }
+        public static MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon, MessageBoxResult defaultResult)
+        {
+            MMBox myBox = new MMBox();
+            myBox.Content.Text = messageBoxText;
+            myBox.Title = caption;
+            myBox.setButtons(button, defaultResult);
+            myBox.setIcon(icon);
+            myBox.ShowDialog();
+            return result;
+        }
+
+        //public static MessageBoxResult Show(Window owner, string messageBoxText);
+        //public static MessageBoxResult Show(Window owner, string messageBoxText, string caption);
+        //public static MessageBoxResult Show(Window owner, string messageBoxText, string caption, MessageBoxButton button);
+        //public static MessageBoxResult Show(Window owner, string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon);
+        //public static MessageBoxResult Show(Window owner, string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon, MessageBoxResult defaultResult);
+        //public static MessageBoxResult Show(Window owner, string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon, MessageBoxResult defaultResult, MessageBoxOptions options);
+
+    }
+}
